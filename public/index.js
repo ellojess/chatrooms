16ea3f8664ab95dcf40e544a3823967c45317ef8
Jess <jtrinh619@gmail.com>
2021-05-01 22:05:43 -0700

    Users can join chat and send messages

--- public/index.js
+++ public/index.js
@@ -1,23 +1,51 @@
-// index.js
+//index.js
 
 $(document).ready(()=>{
   const socket = io.connect();
+
+  //Keep track of the current user
+  let currentUser;
 
   $('#create-user-btn').click((e)=>{
     e.preventDefault();
     if($('#username-input').val().length > 0){
       socket.emit('new user', $('#username-input').val());
+      // Save the current user when created
+      currentUser = $('#username-input').val();
       $('.username-form').remove();
-      // Have the main page visible
       $('.main-container').css('display', 'flex');
+    }
+  });
+
+  $('#send-chat-btn').click((e) => {
+    e.preventDefault();
+    // Get the message text value
+    let message = $('#chat-input').val();
+    // Make sure it's not empty
+    if(message.length > 0){
+      // Emit the message with the current user to the server
+      socket.emit('new message', {
+        sender : currentUser,
+        message : message,
+      });
+      $('#chat-input').val("");
     }
   });
 
   //socket listeners
   socket.on('new user', (username) => {
     console.log(`${username} has joined the chat`);
-    // Add the new user to the online users div
     $('.users-online').append(`<div class="user-online">${username}</div>`);
+  })
+
+  //Output the new message
+  socket.on('new message', (data) => {
+    $('.message-container').append(`
+      <div class="message">
+        <p class="message-user">${data.sender}: </p>
+        <p class="message-text">${data.message}</p>
+      </div>
+    `);
   })
 
 })
