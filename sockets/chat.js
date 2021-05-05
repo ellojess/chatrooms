2943dec2bc4575a2af49aa5517d03f91be41ba86
Jess <jtrinh619@gmail.com>
2021-05-04 18:11:06 -0700

    Initial new channel

--- sockets/chat.js
+++ sockets/chat.js
@@ -30,4 +30,9 @@ module.exports = (io, socket, onlineUsers) => {
     io.emit('user has left', onlineUsers);
   });
 
+  // when new channel button is pressed 
+  socket.on('new channel', (newChannel) => {
+    console.log(newChannel);
+  });
+
 }
